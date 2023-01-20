6e4dba527f21935b2108bef0e041c3b233fb6816
Farid Nizam <faridsaja12@gmail.com>
2023-01-20 20:34:18 +0700

    Update domain

--- domain.js
+++ domain.js
@@ -3,7 +3,7 @@
  * 'Source': 'Target'
  */
 export default {
-  'farid.cyou': 'www.farid.cyou',
+  'www.farid.cyou': 'farid.cyou',
   'yuiistic.my.id': 'www.farid.cyou',
   'onolune.my.id': 'www.farid.cyou',
   'www.fwb.my.id': 'fwb.my.id'
